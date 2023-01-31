3f5e5831859a9edd83a3cb0f478c42daa5708bf1
Peter-Is-Be <123280113+Peter-Is-Be@users.noreply.github.com>
2023-01-31 18:49:28 +0800

    save

--- src/app/alien/ebe1/chapt/ch01/ch01.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector: "app-ch01",
-  templateUrl: "./ch01.component.html",
-  styleUrls: ["./ch01.component.scss"],
-})
-export class Ch01Component {}
--- src/app/alien/ebe1/chapt/ch02/ch02.component.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector: "app-ch02",
-  templateUrl: "./ch02.component.html",
-  styleUrls: ["./ch02.component.scss"],
-})
-export class Ch02Component {}
